ef82b846c0048d83c02de1f2419236734464387e
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-05-10 13:21:51 +0000

    Update index.js

--- server/functions/index.js
+++ server/functions/index.js
@@ -6,11 +6,29 @@ admin.initializeApp();
 
 // Send notification to new users
 exports.welcomeNewUser = functions.auth.user().onCreate((record, context) => {
-    
+    // Get the user login credentials
+    var username = record.displayName;
+    var email = record.email;
+    var key = record.uid;
+    var timestamp = context.timestamp;
+
+    // Store user's information in the database
+    return admin.firestore().collection('customers').doc(key)
+        .set({
+            email,
+            username,
+            key,
+            timestamp
+        }).then(() => {
+            return console.log('new customer information was stored in the database successfully');
+        }).catch(error => {
+            if (error) {
+                return console.log(error.message);
+            }
+        });
 });
 
 // Send notification containing user purchase receipts
 
 
 // Notify users of new items in the catalogues
-
