d28886201e17f14a3b4b89a9fe7927979dcef219
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-07-30 17:11:30 +0000

    Removed old project

--- mobile/app/src/main/java/io/codelabs/xhandieshub/view/recyclerview/EmptyViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package io.codelabs.xhandieshub.view.recyclerview
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class EmptyViewHolder(val v: View) : RecyclerView.ViewHolder(v)
--- mobile/app/src/main/java/io/codelabs/xhandieshub/view/recyclerview/OnItemClickListener.kt
+++
@@ -1,6 +0,0 @@
-package io.codelabs.xhandieshub.view.recyclerview
-
-interface OnItemClickListener<Item> {
-
-    fun onClick(item: Item, isLong: Boolean = false)
-}
